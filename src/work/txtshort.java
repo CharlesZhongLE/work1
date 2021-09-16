0888e5467e5fe19fc8d4092b3e77f21f9bd6b09d
3119005488 <3200578592@qq.com>
2021-09-16 20:01:27 +0800

    2

---
+++ src/work/txtshort.java
@@ -0,0 +1,21 @@
+package work;
+
+public class txtshort extends Exception {
+
+    public txtshort() {
+        super();
+    }
+
+    public txtshort(String message) {
+        super(message);
+    }
+
+    public txtshort(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public txtshort(Throwable cause) {
+        super(cause);
+    }
+
+}
